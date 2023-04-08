0b6cb663e67d3c0f0935ffc6f4a397e168ee75f3
BernardoKaluza <bernardokaluza@ua.pt>
2023-04-08 15:59:50 +0100

    fix edit bug

--- webApp/webApp/settings.py
+++ webApp/webApp/settings.py
@@ -115,14 +115,9 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
-
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'app/static'
 (STATIC_ROOT)
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
